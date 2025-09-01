09314116d4b4fb2f696212aa16f35c73f99e1184
gauriagwl864 <gouriagarwal864@gmail.com>
2025-09-01 20:13:12 +0530

    first commit

---
+++ classwork/argument.java
@@ -0,0 +1,8 @@
+public class argument {
+    public static void main(String[] args) {
+        System.out.println("NUmber of arguments:"+args.length);
+        for(int i=0;i<args.length;i++){
+            System.out.println("Arguments"+i+":"+args[i]);
+        }
+    } 
+}
