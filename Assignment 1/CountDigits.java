eb909afd51bb0c4c54eb5d2ad9b8b5b8aeeed9ae
gauriagwl864 <gouriagarwal864@gmail.com>
2025-09-02 08:33:46 +0530

    First Assignment

--- Assignment 1/CountDigits.java
+++ Assignment 1/CountDigits.java
@@ -1,17 +1,15 @@
 public class CountDigits {
     public static void main(String[] args) {
-        int number = 98765;  // You can change this to test other inputs
+        int number = 98765;  
         int count = 0;
 
         int temp = number;
 
-        // Loop to count digits
         while (temp != 0) {
-            temp /= 10;  // Remove the last digit
-            count++;     // Increment count
+            temp /= 10;  
+            count++;    
         }
 
-        // Output the result
         System.out.println("Number of digits in " + number + " is: " + count);
     }
 }
